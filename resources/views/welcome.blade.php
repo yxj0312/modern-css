4ed0cb199d7a4302baeced9ad625a00b03e2e89d
yxj0312 <yanxiajun@hotmail.com>
2018-07-28 22:05:42 +0200

    Ep 1 Piece By Piece - Structure, Resets, and Flexbox

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -9,33 +9,26 @@
 
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css?family=Open+Sans:400,700" rel="stylesheet">
+        <link rel="stylesheet" href="/css/normalize.css">
         <link rel="stylesheet" href="/css/app.css">
-
-        <style>
-            body {
-                font-family: 'Open Sans', sans-serif;
-            }
-
-            header {
-                background-image: linear-gradient(to right, #a25b5b, #692e4c);
-            }
-        </style>
     </head>
     <body>
 
         <header>
-            <div>
-                <h1>XCasts</h1>
+            <div class="container">
+                <div class="header-top">
+                    <h1>XCasts</h1>
 
-                <a href="#">Sign In</a>
+                    <a href="#">Sign In</a>
+                </div>
+
+                <nav>
+                    <a href="">Catalog</a>
+                    <a href="">Series</a>
+                    <a href="">Podcast</a>
+                    <a href="">Discussion</a>
+                </nav>
             </div>
-
-            <nav>
-                <a href="">Catalog</a>
-                <a href="">Series</a>
-                <a href="">Podcast</a>
-                <a href="">Discussion</a>
-            </nav>
         </header>
 
     </body>
