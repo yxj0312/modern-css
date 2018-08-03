bcbf115830a4b2175ef7346035ed9c38b6162834
yxj0312 <yanxiajun@hotmail.com>
2018-08-03 22:58:53 +0200

    Ep7 - Pricing Selection Design - Add more logos

--- resources/views/plan.blade.php
+++ resources/views/plan.blade.php
@@ -1,21 +1,19 @@
 {{-- A Single Plan --}}
 <div class="flex flex-col justify-around shadow p-6 mx-4 w-1/4 text-center">
     <div class="flex justify-between mb-6">
-        <h5 class="text-grey-darker uppercase tracking-tight">Monthly</h5>
-        {{-- <h5 class="text-grey-darker uppercase tracking-tight">{{ $name }}</h5> --}}
+        <h5 class="text-grey-darker uppercase tracking-tight">{{ $name }}</h5>
 
 
         <a href="#" class="no-underline text-blue flex items-baseline font-bold leading-none">
                     <span class="text-lg">$</span>
-                    <span class="text-4xl">15</span>
+                    <span class="text-4xl">{{ $price }}</span>
                 </a>
     </div>
 
-    <img class="w-1/2 h-auto self-center" src="/image/marketing.png" alt="Monthly Plan">
-    {{-- <img class="w-1/2 h-auto self-center" src="/image/marketing.png" alt="{{ $name }} Plan"> --}}
+    <img class="w-1/2 h-auto self-center" src="{{ $url }}" alt="{{ $name }} Plan">
 
     <p class="mb-8 py-4 text-grey-darkest leading-normal">
-        Still undecided? Ease in with a monthly plan that can be canceled in ten seconds.
+        {{ $description }}
     </p>
 
     <a href="#" class="btn btn-blue font-semibold">Start Learning</a>
--- resources/views/pricing.blade.php
+++ resources/views/pricing.blade.php
@@ -14,14 +14,33 @@
 <body class="font-body p-6">
     <div class="container mx-auto">
        <div class="flex">
-            @include('plan')
-            {{-- @include('plan',['name' => 'Monthly']) --}}
+            @include('plan',[
+                'name' => 'Monthly', 
+                'price' => '15', 
+                'description' => 'Still undecided? Ease in with a monthly plan that can be canceled in ten seconds.',
+                'url'=>'/image/marketing.png'
+                ])
 
-            @include('plan')
+            @include('plan',[
+                'name' => 'Yearly', 
+                'price' => '99', 
+                'description' => "This is your career we're takling about. Go all in, and save 20% off the monthly rate.",
+                'url'=>'/image/pay-per-click.png'
+                ])
 
-            @include('plan')
+            @include('plan',[
+                'name' => 'Forever', 
+                'price' => '350', 
+                'description' => "We get it. Not everyone loves subscriptions. Pay once, and access Laracasts forever.",
+                'url'=>'/image/project.png'
+                ])
 
-            @include('plan')
+            @include('plan',[
+                'name' => 'Teams', 
+                'price' => '300', 
+                'description' => "Follow in the steps of thousands of compaines who have elevated their teams to the next level.",
+                'url'=>'/image/web-optimization.png'
+                ])
        </div>
     </div>
 </body>
